4382a831517a993f7071d9577cd5965405e11a7d
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-07-24 11:32:54 +0300

    "Added drag-and-drop functionality to NoteCard component"

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -2,9 +2,12 @@ import { useEffect, useRef, useState } from "react";
 import Trash from '../icons/Trash';
 
 const NoteCard = ({ note }) => {
-    const position = JSON.parse(note.position);
+    const [position, setPosition] = useState(JSON.parse(note.position));
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
+
+    let mouseStartPos = { x: 0, y: 0 };
+    const cardRef = useRef(null);
 
     const textAreaRef = useRef(null)
 
@@ -18,8 +21,37 @@ const NoteCard = ({ note }) => {
         current.style.height = "auto"; // Reset the height
         current.style.height = current.scrollHeight + "px"; // Set the new height
     }
+
+    const mouseDown = (e)=> {
+        mouseStartPos.x = e.clientX
+        mouseStartPos.y = e.clientY
+
+        document.addEventListener("mousemove", mouseMove)
+        document.addEventListener("mouseup", mouseUp)
+
+    }
+
+    const mouseMove = (e) => {
+        const mouseMoveDir = {
+            x:mouseStartPos.x - e.clientX,
+            x:mouseStartPos.y - e.clientY
+        }
+        console.log('mouseMoveDir', mouseMoveDir)
+        mouseStartPos.x = e.clientX
+        mouseStartPos.y = e.clientY
+
+        setPosition({
+            x:cardRef.current.offsetLeft - mouseMoveDir.x,
+            x:cardRef.current.offset - mouseMoveDir.y,
+        })
+    }
+    const mouseUp = () => {
+        document.removeEventListener("mousemove", mouseMove);
+        document.removeEventListener("mouseup", mouseUp);
+    };
     return (
         <div
+        ref={cardRef}
             className="card"
             style={{
                 backgroundColor: colors.colorBody,
@@ -28,6 +60,7 @@ const NoteCard = ({ note }) => {
             }}
         >
             <div className="card-header" 
+            onMouseDown={mouseDown}
             style={{ backgroundColor: colors.colorHeader }}>
                 <Trash />
             </div>
